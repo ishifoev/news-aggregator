63f90a80025e05b03688528ece4acc5b849f1b32
Ismoil Shifoev <alex.malikov94@gmail.com>
2024-11-11 09:53:48 +0500

    add comment in user repository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -8,11 +8,25 @@ use Illuminate\Support\Facades\Hash;
 
 class UserRepository implements UserRepositoryInterface
 {
+    /**
+     * Finds a user by their email address.
+     *
+     * @param string $email The email address to search for.
+     * @return User|null The user object if found, otherwise null.
+     */
     public function findByEmail(string $email): ?User
     {
         return User::where('email', $email)->first();
     }
 
+
+    /**
+     * Creates a new user record in the database with the provided data.
+     * Hashes the user's password before storing it for security.
+     *
+     * @param array $data The user data to create a new user record.
+     * @return User The newly created user object.
+     */
     public function create(array $data): User
     {
         $data['password'] = Hash::make($data['password']);
